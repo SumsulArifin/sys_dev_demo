daa714907b6891e1650b508e281fe6f6f2947012
Md.Sumsul Arifin <99475443+SumsulArifin@users.noreply.github.com>
2023-12-09 15:47:19 +0600

    sp
    sp

---
+++ sys_dev/src/main/java/com/example/sys_dev/SysDevApplication.java
@@ -0,0 +1,13 @@
+package com.example.sys_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysDevApplication.class, args);
+	}
+
+}
---
+++ sys_dev/src/test/java/com/example/sys_dev/SysDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sys_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
